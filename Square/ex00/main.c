ad2e0b741c85fcadd9a55354acb04fc2adcd4c9d
morgan.bougeard <morgan.bougeard@MacBook-Air-M4-de-Morgan.local>
2026-09-05 00:24:18 +0200

    Quelques main écrits.

--- Square/ex00/main.c
+++ Square/ex00/main.c
@@ -10,10 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 void	rush(int x, int y);
 
-int	main(void)
+int     main(int argc, char **argv)
 {
-	rush(5, 5);
+	if (argc == 3)
+        rush(atoi(argv[1]), atoi(argv[2]));
 	return (0);
 }
